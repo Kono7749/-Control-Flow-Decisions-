aa9f26356e57a74654d964de443fc4feb1a3191e
Kono <wedoza112@gmail.com>
2025-11-25 22:29:56 +0700

    Add files via upload

---
+++ Lab 1-7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int customerType;
+    float consumption_kwh;
+    float totalBill = 0.0;
+
+    if (scanf("%d %f", &customerType, &consumption_kwh) !=2) {
+        return 1;
+    }
+    if (customerType == 1){
+        if (consumption_kwh <= 100){
+            totalBill = consumption_kwh * 3.0;
+        }else{
+            totalBill = consumption_kwh * 4.0;
+        }
+    } else if (customerType == 2){
+        if (consumption_kwh <= 500){
+            totalBill = consumption_kwh * 5.0;
+        }else{
+            totalBill = consumption_kwh * 6.5;
+    }
+    }else{
+        printf("Invalid Customer Type\n");
+    }
+    if (customerType == 1 || customerType == 2){
+        printf("%.2f\n", totalBill);
+    }
+    return 0;
+
+}
