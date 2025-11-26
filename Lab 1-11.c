04b92c005f7b32358f2a5d6e427c3abefb940394
Kono <wedoza112@gmail.com>
2025-11-26 21:45:29 +0700

    Add files via upload

---
+++ Lab 1-11.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    int categoryCode;
+    float price_before_vat;
+    float totalBill = 0.0;
+    float vatAmount = 0.0;
+
+    printf("Enter Price Before VAT and Category Code (1-3): ");
+
+    if(scanf("%f %d", &price_before_vat, &categoryCode) != 2){
+        printf("Invalid input format.\n");
+        return 1;
+    }
+
+    switch (categoryCode) 
+    {
+    case 1:
+        totalBill = price_before_vat * 1.07;
+        break;
+    case 2:
+        totalBill = price_before_vat;
+        break;
+    case 3:
+        totalBill = price_before_vat * 1.15;
+        break;
+    default:
+        printf("Invalid Category\n");
+        
+        return 0;
+    }
+
+    vatAmount = totalBill - price_before_vat;
+    printf("VAT Amount: %2f\n", vatAmount);
+    printf("Total Price: %2f\n", totalBill);
+
+    return 0;
+}
